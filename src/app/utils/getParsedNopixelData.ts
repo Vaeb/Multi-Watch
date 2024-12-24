20b40eef70825139d259d34ac9b6e9f28fcf6f99
Adam Johnson <admj@amazon.com>
2024-12-24 21:00:58 +0000

    streams num

--- src/app/utils/getParsedNopixelData.ts
+++ src/app/utils/getParsedNopixelData.ts
@@ -31,8 +31,6 @@ const getData = async () => {
     const dataRaw = await fetch(url);
     const data = (await dataRaw.json()) as RemoteLive;
 
-    log("Got data!", data?.streams?.length);
-
     // console.log("[getData]", Object.keys(data));
     return data;
   } catch (err) {
@@ -56,6 +54,8 @@ const queryParsedNopixelData = async () => {
     ),
     useColorsDark,
   };
+
+  log("Got data!", parsed.streams?.length);
 
   return parsed;
 };
